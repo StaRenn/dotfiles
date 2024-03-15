cb1f6405c0162b719ac9f04b7a6adaf0d3d9f9d0
stanislav.golyshev <stanislav.golyshev@onetwotrip.com>
2024-03-15 17:27:28 +0300

    update config

--- nvchad_custom/configs/telescope.lua
+++ nvchad_custom/configs/telescope.lua
@@ -1,72 +1,84 @@
-local previewers = require('telescope.previewers')
+local previewers = require("telescope.previewers")
 
 -- do not preview large files
 local new_maker = function(filepath, bufnr, opts)
-  opts = opts or {}
+	opts = opts or {}
 
-  filepath = vim.fn.expand(filepath)
-  vim.loop.fs_stat(filepath, function(_, stat)
-    if not stat then return end
-    if stat.size > 60000 then
-      return
-    else
-      previewers.buffer_previewer_maker(filepath, bufnr, opts)
-    end
-  end)
+	filepath = vim.fn.expand(filepath)
+	vim.loop.fs_stat(filepath, function(_, stat)
+		if not stat then
+			return
+		end
+		if stat.size > 60000 then
+			return
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
 end
 
 local options = {
-  defaults = {
-    vimgrep_arguments = {
-      "rg",
-      "-L",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-    },
-    prompt_prefix = "   ",
-    selection_caret = "  ",
-    entry_prefix = "  ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "horizontal",
-    layout_config = {
-      horizontal = {
-        prompt_position = "top",
-        preview_width = 0.55,
-        results_width = 0.8,
-      },
-      vertical = {
-        mirror = false,
-      },
-      width = 0.87,
-      height = 0.80,
-      preview_cutoff = 120,
-    },
-    file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = { "node_modules" },
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    path_display = { "truncate" },
-    winblend = 0,
-    border = {},
-    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    color_devicons = true,
-    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = new_maker,
-    mappings = {
-      n = { ["q"] = require("telescope.actions").close },
-    },
-  },
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"-L",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--hidden",
 
-  extensions_list = { "themes", "terms" },
+			-- Exclude some patterns from search
+			"--glob=!**/.git/*",
+			"--glob=!**/.idea/*",
+			"--glob=!**/.vscode/*",
+			"--glob=!**/build/*",
+			"--glob=!**/dist/*",
+			"--glob=!**/yarn.lock",
+			"--glob=!**/package-lock.json",
+		},
+		prompt_prefix = "   ",
+		selection_caret = "  ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		file_ignore_patterns = { "node_modules" },
+		generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+		path_display = { "truncate" },
+		winblend = 0,
+		border = {},
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		-- Developer configurations: Not meant for general override
+		buffer_previewer_maker = new_maker,
+		mappings = {
+			n = { ["q"] = require("telescope.actions").close },
+		},
+	},
+
+	extensions_list = { "themes", "terms" },
 }
 
 return options
--- nvchad_custom/mappings.lua
+++ nvchad_custom/mappings.lua
@@ -2,55 +2,56 @@
 local M = {}
 
 M.telescope = {
-  n = {
-    ["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git branches" },
-  },
+	n = {
+		["<leader>ff"] = { "<cmd> Telescope find_files hidden=true <CR>", "Find files" },
+		["<leader>gb"] = { "<cmd> Telescope git_branches <CR>", "Git branches" },
+	},
 }
 
 M.conform = {
-  n = {
-    ["<leader>fm"] = { "<cmd> Format <CR>", "Format" },
-  },
+	n = {
+		["<leader>fm"] = { "<cmd> Format <CR>", "Format" },
+	},
 }
 
 M.multi = {
-  n = {
-    ["<A-k>"] = { ":call vm#commands#add_cursor_up(0, v:count1) <CR>", "Add cursor up" },
-    ["<A-j>"] = { ":call vm#commands#add_cursor_down(0, v:count1) <CR>", "Add cursor down" },
-  },
+	n = {
+		["<A-k>"] = { ":call vm#commands#add_cursor_up(0, v:count1) <CR>", "Add cursor up" },
+		["<A-j>"] = { ":call vm#commands#add_cursor_down(0, v:count1) <CR>", "Add cursor down" },
+	},
 }
 
 M.copilotChat = {
-  n = {
-    ["<leader>ae"] = { "<cmd>CopilotChatExplain<cr>", "CopilotChat - Explain code" },
-    ["<leader>at"] = {
-      ":CopilotChat Generate tests for current code, output only code, without explanation <cr>",
-      "CopilotChat - Generate tests",
-    },
-    ["<leader>ac"] = { ":CopilotChat Review this code, how can i improve it? <cr>", "CopilotChat - Code review" },
-    ["<leader>ar"] = { ":CopilotChat Refactor this code <cr>", "CopilotChat - Refactor" },
-    ["<leader>ad"] = { ":CopilotChat Debug this code <cr>", "CopilotChat - Debug" },
-  },
+	n = {
+		["<leader>ae"] = { "<cmd>CopilotChatExplain<cr>", "CopilotChat - Explain code" },
+		["<leader>at"] = {
+			":CopilotChat Generate tests for current code, output only code, without explanation <cr>",
+			"CopilotChat - Generate tests",
+		},
+		["<leader>ac"] = { ":CopilotChat Review this code, how can i improve it? <cr>", "CopilotChat - Code review" },
+		["<leader>ar"] = { ":CopilotChat Refactor this code <cr>", "CopilotChat - Refactor" },
+		["<leader>ad"] = { ":CopilotChat Debug this code <cr>", "CopilotChat - Debug" },
+	},
 
-  x = {
-    ["<leader>ax"] = { ":CopilotChatInPlace<cr>", "CopilotChat - Run in-place code" },
-  },
+	x = {
+		["<leader>ax"] = { ":CopilotChatInPlace<cr>", "CopilotChat - Run in-place code" },
+	},
 }
 
 M.dap = {
-  n = {
-    ["<leader>rd"] = { ":RustLsp debuggables <CR> 1 <CR>", "Dap debuggables" },
-    ["<leader>rb"] = { "<cmd> DapToggleBreakpoint <CR>", "Dap toggle breakpoint" },
-    ["<leader>rc"] = { ":lua require('dap').clear_breakpoints() <CR>", "Dap clear breakpoints" },
-    ["<leader>ru"] = { ":lua require('dapui').toggle()<CR>", "Toggle Dap UI" },
-    ["<leader>rt"] = {
-      function()
-        require("dap").terminate()
-        require("dapui").close()
-      end,
-      "Dap terminate",
-    },
-  },
+	n = {
+		["<leader>rd"] = { ":RustLsp debuggables <CR> 1 <CR>", "Dap debuggables" },
+		["<leader>rb"] = { "<cmd> DapToggleBreakpoint <CR>", "Dap toggle breakpoint" },
+		["<leader>rc"] = { ":lua require('dap').clear_breakpoints() <CR>", "Dap clear breakpoints" },
+		["<leader>ru"] = { ":lua require('dapui').toggle()<CR>", "Toggle Dap UI" },
+		["<leader>rt"] = {
+			function()
+				require("dap").terminate()
+				require("dapui").close()
+			end,
+			"Dap terminate",
+		},
+	},
 }
 
 -- later
@@ -71,24 +72,24 @@ M.dap = {
 -- }
 
 M.fugitive = {
-  n = {
-    ["<leader>gg"] = { "<cmd> Git <CR>", "Git status" },
-    ["<leader>gc"] = { "<cmd> Git commit <CR>", "Git commit" },
-    ["<leader>gf"] = { "<cmd> Git fetch <CR>", "Git fetch" },
-    ["<leader>gp"] = { "<cmd> Git push <CR>", "Git push" },
-    ["<leader>ga"] = { "<cmd> Gwrite <CR>", "Git add" },
-    ["<leader>gd"] = { "<cmd> Gvdiffsplit! <CR>", "Git fix merge conflict" },
-  },
+	n = {
+		["<leader>gg"] = { "<cmd> Git <CR>", "Git status" },
+		["<leader>gc"] = { "<cmd> Git commit <CR>", "Git commit" },
+		["<leader>gf"] = { "<cmd> Git fetch <CR>", "Git fetch" },
+		["<leader>gp"] = { "<cmd> Git push <CR>", "Git push" },
+		["<leader>ga"] = { "<cmd> Gwrite <CR>", "Git add" },
+		["<leader>gd"] = { "<cmd> Gvdiffsplit! <CR>", "Git fix merge conflict" },
+	},
 }
 
 M.other = {
-  n = {
-    ["K"] = { ":lua vim.lsp.buf.hover() <CR>", "LSP hover" },
-    ["<leader>ra"] = {
-      ':lua require("nvchad.renamer").open() require("cmp").setup.buffer { enabled = false } <CR>',
-      "LSP rename",
-    },
-  },
+	n = {
+		["K"] = { ":lua vim.lsp.buf.hover() <CR>", "LSP hover" },
+		["<leader>ra"] = {
+			':lua require("nvchad.renamer").open() require("cmp").setup.buffer { enabled = false } <CR>',
+			"LSP rename",
+		},
+	},
 }
 
 return M
